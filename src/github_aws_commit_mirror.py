2157ccf1fb28cacb86bd0d43076553f8d0db5064
Dan Masteller <dan.masteller@pedigreetechnologies.com>
2022-12-01 15:08:13 -0600

    chore: read output (#148968)

--- src/github_aws_commit_mirror.py
+++ src/github_aws_commit_mirror.py
@@ -1,6 +1,6 @@
 import io
 import zipfile
-import subprocess
+from subprocess import PIPE, run
 import os
 import boto3
 import json
@@ -87,18 +87,16 @@ def sync_code_commit_repo(repo_name, def_branch):
             {repo_name} to AWS CodeCommit {BColors.ENDC}",
         flush=True,
     )
-    os.system(
-        "cd {0} && git remote add sync \
-            ssh://{1}@git-codecommit.us-east-1.amazonaws.com/v1/repos/{0}".format(
-            repo_name, AWS_SSH_KEY_ID
-        )
-    )
+    # os.system(
+    #     "cd {0} && git remote add sync \
+    #         ssh://{1}@git-codecommit.us-east-1.amazonaws.com/v1/repos/{0}".format(
+    #         repo_name, AWS_SSH_KEY_ID
+    #     )
+    # )
+    command = "cd {0} && git remote add sync ssh://{1}@git-codecommit.us-east-1.amazonaws.com/v1/repos/{0}".format(repo_name, AWS_SSH_KEY_ID)
+    result = run(command, stdout=PIPE, stderr=PIPE, universal_newlines=True, check = True)
+    print(result.returncode, result.stdout, result.stderr)
     os.system("cd {} && git push sync --mirror".format(repo.name))
-    try:
-        x = subprocess.check_output("cd {0} && git remote add sync ssh://{1}@git-codecommit.us-east-1.amazonaws.com/v1/repos/{0}".format(repo_name, AWS_SSH_KEY_ID),shell=True,stderr=subprocess.STDOUT)
-        print(x)
-    except Exception as e:
-        print("Exception" + e)
     response = codecommit_client.get_repository(repositoryName=repo_name)
     current_branch_name = response["repositoryMetadata"]["defaultBranch"]
     if current_branch_name != def_branch:
