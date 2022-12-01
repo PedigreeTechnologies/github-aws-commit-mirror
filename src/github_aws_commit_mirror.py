30e922874dbb90eebe548709b984c60a557ae3b9
Dan Masteller <dan.masteller@pedigreetechnologies.com>
2022-12-01 13:38:49 -0600

    chore: read output (#148968)

--- src/github_aws_commit_mirror.py
+++ src/github_aws_commit_mirror.py
@@ -9,9 +9,9 @@ import os
 import boto3
 import io
 import zipfile
+from subprocess import Popen, PIPE
 from github import Github
 from github import GithubException
-
 
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
@@ -93,13 +93,16 @@ def sync_code_commit_repo(repo_name, def_branch):
             {repo_name} to AWS CodeCommit {BColors.ENDC}",
         flush=True,
     )
-    os.system(
+    sync = os.system(
         "cd {0} && git remote add sync \
             ssh://{1}@git-codecommit.us-east-1.amazonaws.com/v1/repos/{0}".format(
             repo_name, AWS_SSH_KEY_ID
         )
     )
-    print("Test")
+    p = Popen(sync, stdout=PIPE, stderr=PIPE)
+    stdout, stderr = p.communicate()
+    git_response = stdout.decode("utf-8")
+    print("Git Response: " + git_response)
     os.system("cd {} && git push sync --mirror".format(repo.name))
     response = codecommit_client.get_repository(repositoryName=repo_name)
     current_branch_name = response["repositoryMetadata"]["defaultBranch"]
